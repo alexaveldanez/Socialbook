96b5de20ba29f1fb7759dc74c61c7e7068bafd77
Alex Aveldanez <alexsaveldanez@gmail.com>
2020-08-23 17:11:58 -0700

    added alerts feature

--- SocialBookApp-SPA/src/app/nav/nav.component.ts
+++ SocialBookApp-SPA/src/app/nav/nav.component.ts
@@ -1,36 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../_services/auth.service';
+import { AlertsService } from '../_services/alerts.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private alerts: AlertsService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
-    }, error => {
-      console.log(error);
-    });
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alerts.success('Logged in successfully');
+      },
+      (error) => {
+        this.alerts.error(error);
+      }
+    );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alerts.message('Logged out');
   }
-
 }
--- SocialBookApp-SPA/src/app/register/register.component.ts
+++ SocialBookApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
+
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { AuthService } from '../_services/auth.service';
+import { AlertsService } from '../_services/alerts.service';
 
 @Component({
   selector: 'app-register',
@@ -11,22 +13,21 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alerts: AlertsService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registration successful!');
+      this.alerts.success('Registration successful!');
     }, error => {
-      console.log(error);
+      this.alerts.error(error);
     });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 
 }
