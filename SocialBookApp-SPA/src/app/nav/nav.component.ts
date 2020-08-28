fae3096b56dc21f1c19ae45f5c82d307a6e2ce74
Alex Aveldanez <alexsaveldanez@gmail.com>
2020-08-28 00:50:41 -0700

    fixed logout method error

--- SocialBookApp-SPA/src/app/nav/nav.component.ts
+++ SocialBookApp-SPA/src/app/nav/nav.component.ts
@@ -42,7 +42,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    localStorage.remoteItem('user');
+    localStorage.removeItem('user');
     this.authService.decodedToken = null;
     this.authService.currentUser = null;
     this.alerts.message('Logged out');
